b46206b89d04ea31487c67be7b1c141e7ce167df
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-12-04 18:58:23 +0900

    config 수정

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wp');
+define('DB_NAME', 'kyhfan');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'kyhfan');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'm!nv#Rtisin9');
+define('DB_PASSWORD', 'alslqj_2014');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost:8888');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
