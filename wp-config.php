2cb59b4bcd4344f16e3ad113c5e56625db5e6bbe
onsaemiro8318 <minivertising@minivertisingui-MacBook-Air.local>
2014-12-04 17:57:35 +0900

    wp theme 설정

--- wp-config.php
+++ wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'kyhfan');
+define('DB_NAME', 'wp');
 
 /** MySQL database username */
-define('DB_USER', 'kyhfan');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'alslqj_2014');
+define('DB_PASSWORD', 'm!nv#Rtisin9');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:8888');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
